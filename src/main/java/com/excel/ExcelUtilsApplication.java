d20ea4834da68bd3b980d47babdb614c6eb7704a
Leandro José <leandro_daniel08@hotmail.com>
2024-07-23 00:45:08 +0100

    Project setup

---
+++ src/main/java/com/excel/ExcelUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excel/ExcelUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
